51c807af5e5410be0a3a791de58de09b5cf5be44
Sewar-web <s.maqableh@yahoo.com>
2021-04-14 17:11:19 +0300

    sql2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('dotenv').config();
-
+const pg = require('pg');
 const express = require('express');
 
 const superagent = require('superagent');
@@ -12,11 +12,24 @@ server.set('view engine','ejs');
 
 server.use(express.urlencoded({extended:true}));
 
+const client = new pg.Client( { connectionString: process.env.DATABASE_URL,
+  ssl:{rejectUnauthorized: false
+  }
+} );
 const PORT = process.env.PORT || 3030;
 
 server.get('/',(req,res)=>{
-  res.render('./pages/index');
+
+  let SQL=`SELECT * FROM book;`;
+  client.query(SQL)
+    .then (results=>{
+      res.render('pages/index',{bookarray:results.rows});
+    })
+    .catch(err=>{
+      res.render('error',{error:err});
+    });
 });
+
 
 
 
@@ -28,8 +41,6 @@ function handledData(req ,res)
 {
   // let select=req.body.select;
   let search=req.body.AuthororTitle;
-
-
   const bookURL=`https://www.googleapis.com/books/v1/volumes?q=${search}`;
   superagent.get(bookURL)
     .then(getData => {
@@ -43,6 +54,28 @@ function handledData(req ,res)
 
 }
 
+server.post( '/book', ( req,res )=>{
+  let {title,author,isbn,description,image_url} = req.body;
+  let SQL = `INSERT INTO book (title,author,isbn,description,image_url) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+  let savevalue = [title,author,isbn,description,image_url];
+  console.log( savevalue );
+  client.query( SQL, savevalue )
+    .then( result =>{
+      res.redirect(`/book/${result.rows[0].id}`);
+    } );
+} );
+
+server.get( '/book/:id', ( req,res )=>{
+  console.log(req.params);
+  let SQL = `SELECT * FROM book WHERE id=$1;`;
+  let savevalue = [req.params.id];
+  client.query( SQL , savevalue )
+    .then( result =>{
+      res.render( 'pages/book/detail', {bookarray:result.rows[0]} );
+
+    } );
+} );
+
 
 function Books(getData)
 {
@@ -50,13 +83,17 @@ function Books(getData)
   this.author=(getData.volumeInfo.authors) ? getData.volumeInfo.authors : 'there is no data';
   this.description=(getData.volumeInfo.description) ? getData.volumeInfo.description : 'there is no data' ;
   // (getData.volumeInfo.imageLinks) ? getData.volumeInfo.imageLinks.smallThumbnail :`https://i.imgur.com/J5LVHEL.jpg` ;
-  this.img=getData.volumeInfo.imageLinks.thumbnail;
+  this.image_url=getData.volumeInfo.imageLinks.thumbnail;
+  this.isbn = (getData.volumeInfo.industryIdentifiers) ?getData.volumeInfo.industryIdentifiers[0,1].identifier : 'do not have data' ;
 }
 
 server.get('*',(req,res)=>{
   res.status(404).send('There Somthing Error');
 });
 
-server.listen(PORT,()=>{
-  console.log(`Listening on PORT ${PORT}`);
-});
+client.connect()
+  .then( () => {
+    server.listen( PORT, ()=>{
+      console.log( `listening on PORT ${PORT}` );
+    } );
+  } );
